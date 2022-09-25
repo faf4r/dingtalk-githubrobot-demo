1d28ac69ade6538ddbad9a84d68c69a50746fbcc
TMFfa <81461670+TMFfa@users.noreply.github.com>
2022-09-25 12:18:05 +0800

    Delete GitHubdemo.py

--- GitHubdemo.py
+++
@@ -1,5 +0,0 @@
-import requests
-api = 'https://oapi.dingtalk.com/robot/send?access_token=a1e70c2bba8848f0f1da5a84232538d5a58bff24cb3319ddea6974d50a55cf45'
-
-r = requests.post(api, data={"msgtype": "text","text": {"content":"我就是我, 是不一样的烟火"}})
-print(r.text)
